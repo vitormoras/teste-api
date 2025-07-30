3b00d06d76e304a687df8297300916e814a4b891
vitormoras <vitor.moras@hotmail.com>
2025-07-30 14:23:48 -0300

    configuração de static (load static)

--- projeto/settings.py
+++ projeto/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
+# STATICFILES_DIRS = [] Para adicionar arquivos css de uma base de fora do APP
+# STATICROOT = BASE_DIR / 'static' Para setar os caminhos para as pastas estaticas em produção (servidor)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

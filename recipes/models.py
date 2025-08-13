cd54b5986dbc25752fee84e32a64ac3eddc0579a
vitormoras <vitor.moras@hotmail.com>
2025-08-13 15:15:26 -0300

    modelo de categoria e receitas criados

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    serving_time = models.IntegerField()
+    serbing_unit = models.CharField(max_length=65)
+    preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
+    create_at = models.DateTimeField(
+        auto_now_add=True
+    )  # cria a data no momento da criação sem alteração depois
+    update_at = models.DateTimeField(
+        auto_now=True
+    )  # atualiza o campo automaticamente depois de salvar
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to="recipes/cover/%Y/%m/%d/")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)

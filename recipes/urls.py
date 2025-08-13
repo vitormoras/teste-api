0fccf483cb6fa1aac10b413ee91a1aa8ff70b3ff
vitormoras <vitor.moras@hotmail.com>
2025-08-13 10:08:51 -0300

    ajuste nos names_apps e adição de variavel em factory para alternar path id em cada receita

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 
+app_name = "recipes"
+
 urlpatterns = [
-    path("", views.pag_home, name="recipes-home"),
-    path("recipes/<int:id>/", views.pag_recipe, name="recipes-info"),
+    path("", views.pag_home, name="home"),
+    path("recipes/<int:id>/", views.pag_recipe, name="info"),
 ]
